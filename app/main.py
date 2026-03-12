a45f68083af420a582c8bac8a994f2e421d84185
ankit-09999 <ankit.kumar@eatechpvtltd.com>
2026-03-12 11:06:40 +0530

    add the backend of the profile building

--- app/main.py
+++ app/main.py
@@ -10,9 +10,11 @@ app = FastAPI(
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
+    origins = [str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS]
+    print(f"INFO:     CORS enabled for origins: {origins}")
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
